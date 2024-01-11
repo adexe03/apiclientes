13c4a08a6c11674b9186ba73bcb79e6c16f8bc85
Adexe Reyes <adexe.reyes.cb@gmail.com>
2024-01-11 20:06:29 +0000

    functions edit and create clients

--- public/js/clientes.js
+++ public/js/clientes.js
@@ -1,5 +1,9 @@
 const HOST = 'http://localhost:3000/api/';
 const tbody = document.querySelector('tbody');
+const fieldID = document.getElementById('id');
+const fieldNAME = document.getElementById('nombre');
+const fieldLASTNAME = document.getElementById('apellidos');
+const fieldLOCATION = document.getElementById('localidad');
 
 function loadClients() {
   fetch(HOST + 'clientes')
@@ -15,12 +19,97 @@ function loadClients() {
           <td>${client.nombre}</td>
           <td>${client.apellidos}</td>
           <td>${client.localidad}</td>
-          <td></td>
+          <td>
+            <img src='img/editar.png' onclick="loadClient(${client.id})">
+            <img src='img/eliminar.png' onclick="removeClient(${client.id})">
+
+          </td>
         `;
         tbody.append(tr);
       });
     });
   tbody.innerHTML = "<tr><td colspan=\"5\">Cargando datos...</td></tr>";
+}
+
+function loadClient(id) {
+  fetch(HOST + 'clientes/' + id)
+    .then(res => res.json())
+    .then(json => {
+      fieldID.value = json.data.id;
+      fieldNAME.value = json.data.nombre;
+      fieldLASTNAME.value = json.data.apellidos;
+      fieldLOCATION.value = json.data.direccion.localidad;
+    });
+}
+
+function removeClient(id) {
+  console.log('eliminando', id);
+  let requestOptions = {
+    method: "DELETE"
+  };
+  fetch(HOST + 'clientes/' + id, requestOptions)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      loadClients();
+    })
+}
+
+function createClient() {
+  let name = fieldNAME.value;
+  let lastName = fieldLASTNAME.value;
+  let location = fieldLOCATION.value;
+  let client = {
+    "nombre": name,
+    "apellidos": lastName,
+    "direccion": {
+      "localidad": location
+    }
+  };
+
+  let requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(client)
+  };
+
+  fetch(HOST + 'clientes/', requestOptions)
+    .then(res => res.json())
+    // No haría falta
+    .then(json => {
+      console.log(json);
+      loadClients();
+    });
+}
+
+function editClient() {
+  let id = fieldID.value;
+  console.log('Modificando...');
+  let client = {
+    nombre: fieldNAME.value,
+    apellidos: fieldLASTNAME.value,
+    direccion: {
+      localidad: fieldLOCATION.value
+    }
+  };
+
+  let requestOptions = {
+    method: 'PUT',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(client)
+  }
+
+  fetch(HOST + 'clientes/' + id, requestOptions)
+    .then(res => res.json())
+    // No haría falta
+    .then(json => {
+      console.log(json);
+      loadClients();
+    });
 }
 
 loadClients();
